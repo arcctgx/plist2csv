6af1d5606ebc3cc810e9c29a039bd511a7cd14fb
arcctgx <arcctgx@poczta.onet.pl>
2020-07-12 01:04:45 +0200

    get input and output files from command line
    Take source xml and target csv from command line. It's simplistic,
    but does the job. argparse would be overkill.

--- plistparse.py
+++ plistparse.py
@@ -4,48 +4,53 @@ import sys
 import plistlib
 import csv
 
-with open("apple_music_library_excerpt.xml", "rb") as fp:
+if len(sys.argv) != 3:
+    print("usage: {} <input.xml> <output.csv>".format(sys.argv[0]))
+    sys.exit(1)
+
+with open(sys.argv[1], "rb") as fp:
     pl = plistlib.load(fp)
 
-csv.register_dialect("UniversalScrobbler", delimiter=',', lineterminator='\n', quotechar='"', quoting=csv.QUOTE_ALL)
-writer = csv.writer(sys.stdout, dialect="UniversalScrobbler")
+with open(sys.argv[2], "w") as fc:
+    csv.register_dialect("UniversalScrobbler", delimiter=',', lineterminator='\n', quotechar='"', quoting=csv.QUOTE_ALL)
+    writer = csv.writer(fc, dialect="UniversalScrobbler")
 
-for t in pl["Tracks"]:
-    try:
-        title = pl["Tracks"][t]["Name"]
-    except KeyError:
-        print("Record {} is missing mandatory title information, skipping!".format(t), file=sys.stderr)
-        continue
+    for t in pl["Tracks"]:
+        try:
+            title = pl["Tracks"][t]["Name"]
+        except KeyError:
+            print("Record {} is missing mandatory title information, skipping!".format(t), file=sys.stderr)
+            continue
 
-    try:
-        artist = pl["Tracks"][t]["Artist"]
-    except KeyError:
-        print("Record {} is missing mandatory artist information, skipping!".format(t), file=sys.stderr)
-        continue
+        try:
+            artist = pl["Tracks"][t]["Artist"]
+        except KeyError:
+            print("Record {} is missing mandatory artist information, skipping!".format(t), file=sys.stderr)
+            continue
 
-    try:
-        album = pl["Tracks"][t]["Album"]
-    except KeyError:
-        album = ""
+        try:
+            album = pl["Tracks"][t]["Album"]
+        except KeyError:
+            album = ""
 
-    try:
-        album_artist = pl["Tracks"][t]["Album Artist"]
-    except KeyError:
-        album_artist = ""
+        try:
+            album_artist = pl["Tracks"][t]["Album Artist"]
+        except KeyError:
+            album_artist = ""
 
-    try:
-        length = int(pl["Tracks"][t]["Total Time"]/1000)
-    except KeyError:
-        length = ""
+        try:
+            length = int(pl["Tracks"][t]["Total Time"]/1000)
+        except KeyError:
+            length = ""
 
-    try:
-        play_count = pl["Tracks"][t]["Play Count"]
-    except KeyError:
-        print("Failed to get play count for record {}, skipping!".format(t), file=sys.stderr)
-        continue
-    else:
-        if play_count == 0:
-            print("Zero plays for record {}!".format(t), file=sys.stderr)
+        try:
+            play_count = pl["Tracks"][t]["Play Count"]
+        except KeyError:
+            print("Failed to get play count for record {}, skipping!".format(t), file=sys.stderr)
+            continue
+        else:
+            if play_count == 0:
+                print("Zero plays for record {}!".format(t), file=sys.stderr)
 
-    for _ in range(play_count):
-        writer.writerow([artist, title, album, "", album_artist, length])
+        for _ in range(play_count):
+            writer.writerow([artist, title, album, "", album_artist, length])
