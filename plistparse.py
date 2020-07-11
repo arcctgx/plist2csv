653bb5438bf2e5cdb3920576d2fbdc8e4eefab2f
arcctgx <arcctgx@poczta.onet.pl>
2020-07-11 22:39:12 +0200

    more graceful exception handling
    Don't crash and burn when title or artist is missing - skip that record
    and go on.
    
    Print warnings to stderr when artist, title or play count is missing,
    or when play count is zero.

--- plistparse.py
+++ plistparse.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
+import sys
 import plistlib
 
 with open("apple_music_library_excerpt.xml", "rb") as fp:
     pl = plistlib.load(fp)
 
 for t in pl["Tracks"]:
-    # mandatory fields, don't handle exceptions, let it crash and burn if info is missing
-    title = pl["Tracks"][t]["Name"]
-    artist = pl["Tracks"][t]["Artist"]
-    # there's no way to get timestamps of individual plays
-    timestamp = ""
+    try:
+        title = pl["Tracks"][t]["Name"]
+    except KeyError:
+        print("Record {} is missing mandatory title information, skipping!".format(t), file=sys.stderr)
+        continue
+
+    try:
+        artist = pl["Tracks"][t]["Artist"]
+    except KeyError:
+        print("Record {} is missing mandatory artist information, skipping!".format(t), file=sys.stderr)
+        continue
 
     try:
         album = pl["Tracks"][t]["Album"]
@@ -30,7 +37,11 @@ for t in pl["Tracks"]:
     try:
         play_count = pl["Tracks"][t]["Play Count"]
     except KeyError:
-        play_count = 0
+        print("Failed to get play count for record {}, skipping!".format(t), file=sys.stderr)
+        continue
+    else:
+        if play_count == 0:
+            print("Zero plays for record {}!".format(t), file=sys.stderr)
 
     for _ in range(play_count):
-        print('"{}", "{}", "{}", "{}", "{}", "{}"'.format(artist, title, album, timestamp, album_artist, length))
+        print('"{}", "{}", "{}", "{}", "{}", "{}"'.format(artist, title, album, "", album_artist, length))
